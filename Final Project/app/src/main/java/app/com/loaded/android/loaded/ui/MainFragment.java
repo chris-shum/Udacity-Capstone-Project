d4765c4dfb61bf4df1056fe45774173df90eba47
chris-shum <shum.chris8@gmail.com>
2016-12-24 16:24:11 -0500

    Admob added

--- Final Project/app/src/main/java/app/com/loaded/android/loaded/ui/MainFragment.java
+++ Final Project/app/src/main/java/app/com/loaded/android/loaded/ui/MainFragment.java
@@ -6,6 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 import app.com.loaded.android.loaded.R;
 
 
@@ -22,7 +26,15 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         // TODO: 12/14/16 constraint layout and add homepage stuff
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        MobileAds.initialize(getActivity().getApplicationContext(), "ca-app-pub-4193213438982161~1829803539");
+        AdView mAdView = (AdView) view.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        return view;
     }
 
 }
