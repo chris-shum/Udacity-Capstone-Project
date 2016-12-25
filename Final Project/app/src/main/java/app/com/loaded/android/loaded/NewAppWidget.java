ce60aeee5e4bcd37435af3a2495a278c7aca54bc
chris-shum <shum.chris8@gmail.com>
2016-12-25 18:47:04 -0500

    Widget function done

--- Final Project/app/src/main/java/app/com/loaded/android/loaded/NewAppWidget.java
+++ Final Project/app/src/main/java/app/com/loaded/android/loaded/NewAppWidget.java
@@ -1,43 +1,74 @@
 package app.com.loaded.android.loaded;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.widget.RemoteViews;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class NewAppWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        CharSequence widgetText = "RAR";
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
-
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
+
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+
+            DateFormat day = new SimpleDateFormat("EEEE");
+            Date dateobj = new Date();
+            String currentDay = day.format(dateobj);
+
+            DateFormat time = new SimpleDateFormat("HH:mm:ss");
+            String currentTime = time.format(dateobj);
+
+            String number = "Womp";
+
+            Intent callIntent = new Intent(Intent.ACTION_DIAL);
+            callIntent.setData(Uri.parse("tel:" + "908-264-7222"));
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, callIntent, 0);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+            views.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+            views.setTextViewText(R.id.textView, number);
+
+
+            try {
+                Date currentTimeDate = time.parse(currentTime);
+                Date opening = time.parse("11:00:00");
+                Date closing = time.parse("21:00:00");
+                Date openingSunday = time.parse("13:00:00");
+                Date closingSunday = time.parse("18:00:00");
+
+                if (currentDay.equals("Sunday")) {
+                    if(currentTimeDate.after(openingSunday)&&currentTimeDate.before(closingSunday)){
+                        number = "open";
+                    }else{
+                        number = "closed";
+                    }
+                } else {
+                    if(currentTimeDate.after(opening)&&currentTimeDate.before(closing)){
+                        number = "open";
+                    }else{
+                        number = "closed";
+                    }
+                }
+                views.setTextViewText(R.id.textView, "Loaded is " +number);
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            // Get the layout for the App Widget and attach an on-click listener to the button
+
+            // Tell the AppWidgetManager to perform an update on the current App Widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
-
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
 }
-
